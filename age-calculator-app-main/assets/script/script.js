f1c9b6aba116194041334a631b8758acb38bfb2a
ravi <116646348+rasingh-rgb@users.noreply.github.com>
2023-05-16 10:27:36 +0200

    2nd Last Update
    Implementierung Desktop Version
    Installtion Dayjs

--- age-calculator-app-main/assets/script/script.js
+++ age-calculator-app-main/assets/script/script.js
@@ -1,19 +1,22 @@
 const btn = document.getElementById("submitbtn");
-
+const currentDate = new Date();
 
 btn.addEventListener('click', () => {
     let day = document.getElementById("day").value;
     let month = document.getElementById("month").value;
     let year = document.getElementById("year").value;
 
-    let currentYear = 2023;
-    let currentMonth = 5;
-    let currentDay = 5;
+    let currentYear = currentDate.getFullYear();
+    let currentMonth = currentDate.getMonth();
+    let currentDay = currentDate.getDay();
 
-    document.getElementById("years").innerText = currentYear - year + "YEAR";
-    document.getElementById("months").innerText = currentMonth - month + "MONTHS";
-    document.getElementById("days").innerText = currentDay - day + "DAYS";
+    console.log(currentYear);
 
+    day = currentDay - day;
+    year = currentYear - year;
+    month = currentMonth - month;
 
-
+    document.getElementById("years").innerText = year;
+    document.getElementById("months").innerText = month;
+    document.getElementById("days").innerText = day;
 });
